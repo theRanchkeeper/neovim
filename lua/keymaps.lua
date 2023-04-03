8a61ffe58949149533792c6934266d2d38ac93c1
theRanchkeeper <abhirammj100@gmail.com>
2023-04-03 19:35:11 +0530

    Lazy homescreen keymap changed

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,6 +1,11 @@
 vim.g.mapleader = ' '
 
+
+--Nvim-tree
 vim.keymap.set('n','<leader>e',vim.cmd.NvimTreeToggle)
+--vim.keymap.set('n','<leader>',vim.cmd("NvimTreeResize +10"))
 
 --Lazy keymap
-vim.keymap.set('n','<C-l>',vim.cmd.Lazy)
+vim.keymap.set('n','<leader>l',vim.cmd.Lazy)
+
+
